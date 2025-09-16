ff56c7b30854dd7e9f0e65e4b531d0561c543ad9
Duy Nguyễn <fansjaki@gmail.com>
2025-09-16 10:31:29 +0700

    rm import annotation

--- ch07_ex3_cart/src/main/java/murach/cart/CartServlet.java
+++ ch07_ex3_cart/src/main/java/murach/cart/CartServlet.java
@@ -4,7 +4,6 @@ import murach.business.LineItem;
 import murach.data.ProductIO;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
